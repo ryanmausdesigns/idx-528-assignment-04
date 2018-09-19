d7477551efe673ea97dd11ade6970868c0f8fc2b
Ryan Maus <rmaus@id.iit.edu>
2018-09-19 15:43:11 -0500

    Ali can you give me a hint to make the switch statement work?

---
+++ script.js
@@ -0,0 +1,40 @@
+const name = "Ryan";
+var x = 29;
+let age = x;
+age *= 5; 
+console.log (age);
+const last_name = "Maus";
+// Full name is required to be last name, first. 
+full_name = last_name + ", " + name; 
+console.log (full_name); 
+let fruits = [
+    "Apples", 
+    "Oranges", 
+    "Pears", 
+    "Bananas", 
+    "Cherries",
+];
+
+if (length < 5 ) {
+    console.log ( "only a few fruits!" )
+ } else if (length > 5 ) {
+    console.log ( "nice list of fruits.")
+ } else {
+    console.log ("FRUUUITS!!")
+ };
+
+ var first = fruits[0];
+ var first_price = price[0];
+
+ let fruits.price = [
+    "$.99",
+    "$1.05",
+    "$1.05",
+    "$0.49",
+    "$$3.01"
+ ];
+ 
+ switch (fruits) {
+    case first:
+      console.log(first_price);
+  }; 
