2353363445b368ea256c367a30959de422a1dac8
Ryan Maus <rmaus@id.iit.edu>
2018-09-20 22:57:43 -0500

    Submitting assignment for week 4

--- script.js
+++ script.js
@@ -15,26 +15,30 @@ let fruits = [
     "Cherries",
 ];
 
-if (length < 5 ) {
+if (fruits.length < 5 ) {
     console.log ( "only a few fruits!" )
- } else if (length > 5 ) {
+ } else if (fruits.length > 5 ) {
     console.log ( "nice list of fruits.")
  } else {
     console.log ("FRUUUITS!!")
  };
 
- var first = fruits[0];
- var first_price = price[0];
-
- let fruits.price = [
-    "$.99",
-    "$1.05",
-    "$1.05",
-    "$0.49",
-    "$$3.01"
- ];
+var askFruit = prompt("Enter fruit's name for price information")
  
- switch (fruits) {
-    case first:
-      console.log(first_price);
-  }; 
+ switch (askFruit) {
+    case "Apples":
+        console.log("1.99");
+        break; 
+    case "Oranges":
+        console.log("2.99"); 
+        break;
+    case "Pears":
+        console.log("$1.50");
+        break; 
+    case "Bananas": 
+        console.log("$0.49");
+        break;
+    case "Cherries": 
+        console.log("$3.01");
+        break;
+ }
